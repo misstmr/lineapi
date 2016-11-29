1eaf8523e8e9f9477de5e9cc71277d47c7d0f6bd
sittipong <mr.misstmr@gmail.com>
2016-11-29 15:59:13 +0700

    add ajax sent

--- sentnotify.php
+++ sentnotify.php
@@ -1,43 +1,29 @@
-<?php
-
-$access_token = 'twiWFbmpqX3L7qLnnPNYEhWqCottxucKr7BB082T9AW';
-
-// Validate parsed JSON data
-// Loop through each event
-// Reply only when message sent is in 'text' format
-$text = "sent by notify";
-
-
-// Get replyToken
-$toid = "Uc306e0332ff28d6e2ba20889702f90fd";
-
-// Build message to reply back
-$messages = [
-    'type' => 'text',
-    'text' => $text
-];
-
-// Make a POST Request to Messaging API to reply to sender
-$url = 'https://notify-api.line.me/api/notify';
-$data = [
-   
-    'message' => $text
-];
-$post = 'message=notify 123456789';
-$headers = array('Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result . "\r\n";
 
 
 
-echo "OK";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("button").click(function(){
+        $.post("ajaxpost.php",
+        {
+          access_token: "twiWFbmpqX3L7qLnnPNYEhWqCottxucKr7BB082T9AW",
+          message: "message"
+        },
+        function(data,status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    });
+});
+</script>
+</head>
+<body>
+
+<button>Send an HTTP POST request to a page and get the result back</button>
+
+</body>
+</html>
+
